47a04e31c6ee751773135fa33b9c484f9a5f3c41
liuhongbin1 <1352228477@qq.com>
2018-12-27 11:36:13 +0800

    tijiao

---
+++ src/components/newheader.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<ul>
+			<router-link tag="li" to="/new/">新闻</router-link>
+			<router-link tag="li" to="/new/trailer">预告片</router-link>
+			<router-link tag="li" to="/new/toplist">排行榜</router-link>
+			<router-link tag="li" to="/new/review">影评</router-link>
+		</ul>
+	</div>
+</template>
+<script></script>
+<style>
+	
+</style>
